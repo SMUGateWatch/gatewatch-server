07cd672e70184697903f8d7c108359cb986b661f
John Bryan Delda <mightypirates174@gmail.com>
2020-11-23 23:47:19 +0800

    added some changes

--- app.js
+++ app.js
@@ -5,17 +5,22 @@ const app = express()
 const server = http.createServer(app)
 const io = socketIO(server)
 const data = require('./models/registered')
+
+
 io.on('connected',(socket)=>{
     socket.on("scanId",(id)=>{
         const isIdExist = data.filter(d => (d.schoolId === id)? true : false);
         if (isIdExist) socket.emit("permitId",JSON.stringify(1));
         if (isIdExist) socket.emit("permitId",JSON.stringify(0));
-        
-    })
-    socket.on("storeData",(data)=>{
-        
-        
-    })
+        console.log("scanId is executed...");
+    });
+    
+    socket.on("manualControlLift",(control)=>{
+        let controlData = control.json()
+        if(controlData.lift.up) socket.emit("iotBarrierUp",JSON.stringify(""))
+        if(controlData.lift.down) socket.emit("iotBarrierUp",JSON.stringify(""))
+        console.log("manualControlLift is executed...");
+    });
 })
 module.exports = server;
 
