f0bfb5e01906615661b6f58f1b9fdcc8b6e1131b
John Bryan Delda <mightypirates174@gmail.com>
2020-11-23 22:10:35 +0800

    added some changes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const http = require('http')
-const socketIO = require('socketio')
+const socketIO = require('socket.io')
 const app = express()
 const server = http.createServer(app)
 const io = socketIO(server)
@@ -10,6 +10,10 @@ io.on('connected',(socket)=>{
         const isIdExist = data.filter(d => (d.schoolId === id)? true : false);
         if (isIdExist) socket.emit("permitId",JSON.stringify(1));
         if (isIdExist) socket.emit("permitId",JSON.stringify(0));
+        
+    })
+    socket.on("storeData",(data)=>{
+        
         
     })
 })
