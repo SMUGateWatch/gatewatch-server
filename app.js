aa0f68bdac6341d197781ecc56f2949ca9c2fd74
Peek.A.Bo0 <mightypirates174@gmail.com>
2020-12-10 13:14:34 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-    console.log('received: %s', message.json());
+    console.log('received: %s', message);
     ws.send('something');
   });
  
