41bcbe0169736932ed561ef0ebc31f30269669a3
Peek.A.Bo0 <mightypirates174@gmail.com>
2020-12-10 14:47:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ const wss = new WebSocket.Server({ server });
 
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
-    let event = data.event;
-    let pkg_data = data.data;
-    console.log('received: %s', pkg_data);
+    let parsedData = data.json();
+    console.log('received: %s', parsedData.data.name);
 
     ws.send(JSON.stringify({emitterEvent: "permitID", emitData: "HEYYA"}));
   });
