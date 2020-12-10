39aac2772ca379cc35712c178f2bcf59c25f9be5
Peek.A.Bo0 <mightypirates174@gmail.com>
2020-12-10 11:51:35 +0800

    Added some socket events

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ const wss = new WebSocket.Server({ server });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
     console.log('received: %s', message);
+    
   });
+  ws.on('idPermission', ms => {
+    ws.send('something');
+  })
 
-  ws.send('something');
 });
 
 module.exports = server
